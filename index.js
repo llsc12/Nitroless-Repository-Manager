c86f0b9e3b4c068cde68de636d70ec2091208e3a
llsc12 <42747613+llsc12@users.noreply.github.com>
2021-07-24 13:03:39 +0100

    ok
    idk what this

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const fs = require('fs'), prompt = require('prompt');
+const { X_OK } = require('constants');
+const fs = require('fs'), prompt = require('prompt'), { createCanvas, loadImage } = require('canvas')
 prompt.start()
 console.log('enter emotes directory name')
-prompt.get(['dir', 'RepoName'], (err, result) => {
+prompt.get(['dir', 'RepoName'], async (err, result) => {
   let emoteDir = result.dir
   let folder = fs.readdirSync(`./${emoteDir}`)
   let jsonStructure = 
@@ -22,5 +23,28 @@ prompt.get(['dir', 'RepoName'], (err, result) => {
   jsonStructure["emotes"] = emotes
   fs.writeFileSync('./index.json', JSON.stringify(jsonStructure), 'utf8');
   console.log('Wrote table to index.json')
+
+  folder.forEach(x => {
+    folder[folder.indexOf(x)] = `./${emoteDir}/${x}`
+  })
+  folder.forEach(x => {
+    if (!(x.toLowerCase().endsWith('jpeg') || x.toLowerCase().endsWith('jpg') || x.toLowerCase().endsWith('png') )) return console.log(`Invalid file found at ${x}`)
+    let ext
+    if (x.toLowerCase().endsWith('jpeg') || x.toLowerCase().endsWith('jpg')) ext = 'jpeg'
+    if (x.toLowerCase().endsWith('png')) ext = 'png'
+    let slicelen 
+    if (x.toLowerCase().endsWith('jpeg')) slicelen = 4
+    else slicelen = 3
+    console.log(x)
+    const canvas = createCanvas(48, 48)
+    const ctx = canvas.getContext('2d')
+    loadImage(x).then((image) => {
+      ctx.drawImage(image, 0, 0, 48, 48)
+    })
+    const buff = canvas.toBuffer(`image/${ext}`)
+    let filename = `${x.slice(0, x.length-slicelen)}${ext}`
+    console.log(filename)
+    fs.writeFileSync(filename, buffer)
+  })
   return
-});
+})
